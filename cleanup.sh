211883dae1be4b11509e56ae76a5a5a140614593
cwills <devnull@localhost>
2014-01-21 20:22:16 -0500

    cleanup

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PIDS=""
-RUN_DIR="$HOME/.xmonad/run"
-
-for file in ${RUN_DIR}/*; do 
-  kill -9 `cat $file`
-done
-
--- clock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-  date +'%a %b %d %l:%M%p'
-  sleep 30
-done
