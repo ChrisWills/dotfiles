815e972b1b161719649b68a17cc8349214061621
cwills <cwills.dev@gmail.com>
2018-11-24 22:21:50 -0500

    _

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -545,29 +545,33 @@ controlled by `include'."
                     :action '(("Switch config" . cp/jane-switch-config-action)))
           :buffer "*Switch config*"))
 
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping t
- )
+;;(setq
+;; helm-gtags-ignore-case t
+;; helm-gtags-auto-update t
+;; helm-gtags-use-input-at-cursor t
+;; helm-gtags-pulse-at-cursor t
+;; helm-gtags-prefix-key "\C-cg"
+;; helm-gtags-suggested-key-mapping t
+;; )
 
-(require 'helm-gtags)
-;; Enable helm-gtags-mode
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-c g .") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+;;(require 'helm-gtags)
+;;;; Enable helm-gtags-mode
+;;(add-hook 'dired-mode-hook 'helm-gtags-mode)
+;;(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+;;(add-hook 'c-mode-hook 'helm-gtags-mode)
+;;(add-hook 'c++-mode-hook 'helm-gtags-mode)
+;;(add-hook 'asm-mode-hook 'helm-gtags-mode)
+;;
+;;(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+;;(define-key helm-gtags-mode-map (kbd "C-c g .") 'helm-gtags-select)
+;;(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+;;(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+;;(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+;;(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+;;
+;;(local-unset-key (kbd "SPC f F"))
+;;(define-key evil-normal-state-local-map (kbd "SPC f F") 'counsel-file-jump)
+;;(define-key evil-normal-state-local-map (kbd "SPC a g r") 'counsel-rg)
 
   )
 
